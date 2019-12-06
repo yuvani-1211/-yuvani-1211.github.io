17e99cc9ef88465f16464667355649d8dda0ec26
yuvani-1211 <yuvaranichan5@gmail.com>
2019-12-06 17:50:11 +0530

    web module2

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -2,11 +2,10 @@
 function validform (inputtxt){
 	var numbers = /^[0-9]+$/;
 	var name = document.myform.name.value;
-	var name = document.myform.price.value;
+	var price = document.myform.price.value;
 	var date = document.myform.date.value;
 	if (name == null || name == "") {
 		alert("Name can't be blank");
-		name.focus();
 		return false;
 	} else if ( name.length < 2) {
 		alert("name should have more than 2  characters.");
@@ -14,15 +13,12 @@ function validform (inputtxt){
 	}
 	if (date == null || date == "") {
 		alert("Date of Launcher can't be blank");
-		date.focus();
 		return false;
 	}
     if (inputtxt.value.match(numbers)) {
-		price.focus();
         return true;
     } else {
-        alert('Please input numeric characters only');
-        price.focus();
+        alert("Please input numeric characters only");
         return false;
     }
 		return true;
