a77250b64d0ce6b9e9e17f44a2a99bbe0f91fd90
yuvani-1211 <yuvaranichan5@gmail.com>
2019-12-06 09:36:43 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,29 @@
 // Include truYum form validation functions here
+function validform (inputtxt){
+	var numbers = /^[0-9]+$/;
+	var name = document.myform.name.value;
+	var name = document.myform.price.value;
+	var date = document.myform.date.value;
+	if (name == null || name == "") {
+		alert("Name can't be blank");
+		name.focus();
+		return false;
+	} else if ( name.length < 2) {
+		alert("name should have more than 2  characters.");
+		return false;
+	}
+	if (date == null || date == "") {
+		alert("Date of Launcher can't be blank");
+		date.focus();
+		return false;
+	}
+    if (inputtxt.value.match(numbers)) {
+		price.focus();
+        return true;
+    } else {
+        alert('Please input numeric characters only');
+        price.focus();
+        return false;
+    }
+		return true;
+}
